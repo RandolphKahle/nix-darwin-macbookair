a499554494650da2500ea516f0026bf4e49314d3
Randolph Kahle <randolph.kahle@kahle.org>
2025-08-06 18:39:28 -0500

    "Added Fantastical, DayOne, Nisus Pro, Pages, Numbers, Signal, and What's App"

--- modules/brew.nix
+++ modules/brew.nix
@@ -6,9 +6,20 @@
       # CLI tools if needed (optional)
     ];
 
+    masApps = {
+        Fantastical = 975937182;
+        DayOne = 1055511498;
+        NisusPro = 1429261742;
+        Pages = 409201541;
+        Numbers = 409203825;
+    };
+
+
     casks = [
       "iterm2"
       "brave-browser"
+      "signal"
+      "whatsapp"
     ];
 
     # Automatically install missing brews/casks during `darwin-rebuild`
